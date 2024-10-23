16e8ba3786db52228848f1e5bad284496739c9d4
SkippedSkypiea <fabegabe26+1@gmail.com>
2024-10-22 21:47:35 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+$.post('https://your-function-url', {
+    ip: ip,
+    location: locationData,
+    service_id: 'your_service_id',
+    template_id: 'your_template_id',
+    user_id: 'your_user_id'
+});
 
